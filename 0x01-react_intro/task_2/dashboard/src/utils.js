dae812ba632cf57770c24d18b5a57048775b34e1
Nimah <busayonimota1603@gmail.com>
2023-01-26 13:23:06 +0100

    [Update]: Modify the notifications

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -6,3 +6,7 @@ export function getFooterCopy(isIndex) {
   if (isIndex === true) return 'Holberton School';
   return 'Holberton School main dashboard';
 }
+
+export function getLatestNotification() {
+  return '<strong>Urgent requirement</strong> - complete by EOD';
+}

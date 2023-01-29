31c98c2df4b7ee7d486c7bf62f6be1b1152fc15e
Nimah <busayonimota1603@gmail.com>
2023-01-29 20:15:53 +0100

    [Feats]: Add Javascript files

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+const _ = require('lodash');
+import '../css/main.css';
+
+// import { debounce } from 'lodash';
+
+let click_count = 0;
+
+$('body').append("<div id='logo'> </div>");
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
+
+function updateCounter() {
+  click_count++;
+  $('#count').html(`${click_count} clicks on the button`);
+}
+
+$('button').on('click', _.debounce(updateCounter, 500));

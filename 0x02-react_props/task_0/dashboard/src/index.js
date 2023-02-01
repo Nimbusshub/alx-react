ada74b2d1acf9b41f6925068ee3907060c90553d
Nimah <busayonimota1603@gmail.com>
2023-02-01 22:26:22 +0100

    [Update]:

--- 0x02-react_props/task_0/dashboard/src/index.js
+++ 0x02-react_props/task_0/dashboard/src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
-// import reportWebVitals from './reportWebVitals';
-import Notifications from './Notifications/Notifications';
-
-//cannot redeclare block-scope variable root
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// const root-notifications = ReactDOM.createRoot(document.getElementById('root-notifications'))
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,14 +8,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Notifications />
-  </React.StrictMode>,
-  document.getElementById('root-notifications')
-);
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
